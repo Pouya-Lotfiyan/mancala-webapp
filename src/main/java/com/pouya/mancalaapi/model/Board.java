76f2c41a5f13769b421f08eff79f45147cbd5235
pouya <lotfiyan.p77@gamil.com>
2021-07-15 01:40:03 +0430

    added equals to board

--- src/main/java/com/pouya/mancalaapi/model/Board.java
+++ src/main/java/com/pouya/mancalaapi/model/Board.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "board")
@@ -73,5 +74,18 @@ public class Board {
 
     public void setPits(List<Pit> pits) {
         this.pits = pits;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+        return id == board.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
