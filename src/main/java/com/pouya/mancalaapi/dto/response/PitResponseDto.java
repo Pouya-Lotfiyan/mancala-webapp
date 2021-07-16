55449123bee2a49822bb8b0640f68e5ea24f75d7
pouya <lotfiyan.p77@gamil.com>
2021-07-17 01:12:17 +0430

    added id to response pit

--- src/main/java/com/pouya/mancalaapi/dto/response/PitResponseDto.java
+++ src/main/java/com/pouya/mancalaapi/dto/response/PitResponseDto.java
@@ -6,13 +6,19 @@ public class PitResponseDto {
 
     private long playerId;
 
+    private long id;
+
     private int currentStones;
 
     private boolean isBigPit;
 
+    private int index;
+
     public PitResponseDto(Pit pit){
         this.currentStones = pit.getCurrentStones();
-        this.isBigPit = pit.isBigPick();
+        this.isBigPit = pit.isMancalaPit();
+        this.id = pit.getId();
+        this.index = pit.getIndex();
         this.playerId = pit.getOwner().getId();
     }
 
@@ -40,5 +46,21 @@ public class PitResponseDto {
 
     public void setBigPit(boolean bigPit) {
         isBigPit = bigPit;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 }
